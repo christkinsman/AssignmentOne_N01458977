41a307d2979307b5a232a4960dd6d9670ead8f38
Chris Kinsman <chris.t.kinsman@gmail.com>
2020-09-23 11:52:04 -0400

    Added detail to a citation.

--- AssignmentOne_N01458977/Controllers/HostingCostController.cs
+++ AssignmentOne_N01458977/Controllers/HostingCostController.cs
@@ -15,12 +15,13 @@ namespace AssignmentOne_N01458977.Controllers
         {
             float fortnights = (float)Math.Floor( id / 14 );
             //The idea to add one to fornights in the subtotal "(fortnights + 1) * (float)5.50",
-            //was discussed in class on 19/16/2020.
+            //was discussed in class on 9/16/2020.
             float subTotal = (fortnights + 1) * (float)5.50;
             float HST = (subTotal * (float)1.13) - subTotal;
             float total = subTotal + HST;
 
             //I use ".ToString("c2")" which I found here - https://stackoverflow.com/a/6356381
+            //Answer provided by WraithNath. Last viewed on 9/23/2020.
             return new string[] { 
                                   fortnights + " fortnights at $5.50/FN = " + subTotal.ToString("c2") + " CAD",
                                   "HST 13% = " + HST.ToString("c2") + " CAD",
